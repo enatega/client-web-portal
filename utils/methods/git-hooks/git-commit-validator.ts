f3ed84ebde689f9e1e9a1a5ef78c6d08b22fd181
Umer Farooq <umer_farooq@ninjascode.com>
2024-07-25 15:34:04 +0500

    develop - fix - some:description2 - v3

--- utils/methods/git-hooks/git-commit-validator.ts
+++ utils/methods/git-hooks/git-commit-validator.ts
@@ -2,18 +2,18 @@
 const {execSync} = require('child_process')
 
 const commitMsg = execSync('cat .git/COMMIT_EDITMSG').toString().trim();
-const pattern = /^(\w+) - (build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test|translation|security|changeset) - (.+) - (v\d+)$/;
+const pattern: RegExp = /^(\w+) - (build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test|translation|security|changeset) - (.+) - (v\d+)$/;
 
-if (!pattern.test(commitMsg)) {
+if (!pattern.test(commitMsg ?? "")) {
   console.error('Invalid commit message format.');
   console.error('Format should be: branch - subject - description - version');
   process.exit(1);
 }
-console.log({orignal: commitMsg.match(pattern).slice(1)})
-const [branch, subject, description, version] = commitMsg.match(pattern).slice(1);
 
-// Get the last commit message
-let lastCommitMsg;
+const [branch, subject, description, version] = commitMsg.match(pattern)?.slice(1);
+
+// Get the last commit messageå
+let lastCommitMsg: string | undefined;
 try {
   lastCommitMsg = execSync('git log -1 --pretty=%B').toString().trim();
 } catch (error) {
@@ -25,12 +25,9 @@ if (lastCommitMsg) {
   const lastCommitPattern = /^(\w+) - (build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test|translation|security|changeset) - (.+) - (v\d+)$/;
   const lastCommitMatch = lastCommitMsg.match(lastCommitPattern);
 
-  console.log({lastCommitMatch})
-
   if (lastCommitMatch) {
-    const [, , lastDescription, lastVersion] = lastCommitMatch.slice(1);
+    const [ lastDescription, lastVersion] = lastCommitMatch.slice(1);
 
-      console.log({lastDescription, description, lastVersion, version})
     if (description === lastDescription) {
       console.error('Description must be different from the previous commit.');
       process.exit(1);
