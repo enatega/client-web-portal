8997182d25a71a951fbdc901a94c25dce3dd37c6
Umer Farooq <umer_farooq@ninjascode.com>
2024-07-25 12:46:18 +0500

    implemented eslint, prettier and git hooks

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,7 +7,6 @@
 // refactor: A code change that neither fixes a bug nor adds a feature
 // style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
 // test: Adding missing tests or correcting existing tests
-
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
