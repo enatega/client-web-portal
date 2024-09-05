a5cdeb29c84dab09a0ce3256ae8aab0a6055aee4
Umer Farooq <umer_farooq@ninjascode.com>
2024-09-05 18:50:45 +0500

    develop - changeset - implemented, header, sidebar layouts with some ui changes - v16

--- lib/ui/layouts/protected/main/index.tsx
+++ lib/ui/layouts/protected/main/index.tsx
@@ -1,22 +1,31 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 'use client';
 
+import { LayoutContext } from '@/lib/context/layout-context';
 // Components
 import AppTopbar from '@/lib/ui/global-components/protected/app-bar';
 import AppFooter from '@/lib/ui/global-components/protected/app-footer';
+import Sidebar from '@/lib/ui/global-components/protected/side-bar';
 
 // Interface
 import { ILayoutProvider } from '@/lib/utils/interfaces';
+import { LayoutContextProps } from '@/lib/utils/types';
+import { useContext } from 'react';
 
 const Layout = ({ children }: ILayoutProvider) => {
+  const { isSidebarVisible } = useContext<LayoutContextProps>(LayoutContext);
+
   return (
     <>
       <div className="layout-top-container">
         <AppTopbar />
       </div>
       <div className="layout-main-container">
-        {children}
-        <AppFooter />
+        <Sidebar />
+        <div className={`lg:ml-0 md:ml-${isSidebarVisible ? 64 : 0} p-6`}>
+          {children}
+          <AppFooter />
+        </div>
       </div>
 
       {/* <AppConfig /> */}
