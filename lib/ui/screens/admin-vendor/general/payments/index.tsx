3623191dcb7ba4d4cb0f6b3a61cef946952fd5f6
Talha2502 <tsyed225@gmail.com>
2024-09-25 15:41:45 +0500

    talha - fix - fixed the issues in hedge admin portal again  - v1.4

--- lib/ui/screens/admin-vendor/general/payments/index.tsx
+++ lib/ui/screens/admin-vendor/general/payments/index.tsx
@@ -1,16 +1,11 @@
-// Core
-import { useState } from 'react';
-
 // Components
 import PaymentMain from '@/lib/ui/screen-components/protected/payment/main';
 import PaymentHeader from '@/lib/ui/screen-components/protected/payment/header/screen-header';
 
 export default function PaymentScreen() {
-  const [, setIsAddPaymentVisible] = useState(false);
-
   return (
     <div className="flex flex-col p-3 h-screen overflow-hidden">
-      <PaymentHeader setIsAddPaymentVisible={setIsAddPaymentVisible} />
+      <PaymentHeader />
       <div className="flex-grow overflow-y-auto">
         <PaymentMain />
       </div>
--- lib/ui/screens/super-admin/management/commission-rates/index.tsx
+++ lib/ui/screens/super-admin/management/commission-rates/index.tsx
@@ -1,17 +1,11 @@
-// Core
-import { useState } from 'react';
-
 // Components
 import CommissionRateMain from '@/lib/ui/screen-components/protected/commissionRate/view/main';
 import CommissionRateHeader from '@/lib/ui/screen-components/protected/commissionRate/view/header/screen-header';
 
 export default function CommissionRateScreen() {
-  const [, setIsAddCommissionVisible] = useState(false);
   return (
     <div className="flex flex-col p-3 h-screen overflow-hidden">
-      <CommissionRateHeader
-        setIsAddCommissionRateVisible={setIsAddCommissionVisible}
-      />
+      <CommissionRateHeader />
       <div className="flex-grow overflow-y-auto">
         <CommissionRateMain />
       </div>
