895379ffc2016bcd4c0d3104c7ee7f846dcdfb64
Umer Farooq <umer_farooq@ninjascode.com>
2024-10-08 14:57:57 +0500

    umer - fix - fixing sidebar menu close - v1.0.80

--- app/(protected)/(super-admin)/layout.tsx
+++ app/(protected)/(super-admin)/layout.tsx
@@ -3,16 +3,20 @@ import SUPER_ADMIN_GUARD from '@/lib/hoc/SUPER_ADMIN_GUARD';
 // Layout
 import SuperAdminLayout from '@/lib/ui/layouts/protected/super-admin';
 
+import { useMemo } from 'react';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const ProtectedLayout = SUPER_ADMIN_GUARD(
-    ({ children }: { children: React.ReactNode }) => {
-      return <SuperAdminLayout>{children}</SuperAdminLayout>;
-    }
-  );
+  const ProtectedLayout = useMemo(() => {
+    return SUPER_ADMIN_GUARD(
+      ({ children }: { children: React.ReactNode }) => {
+        return <SuperAdminLayout>{children}</SuperAdminLayout>;
+      }
+    );
+  }, []);
 
   return <ProtectedLayout>{children}</ProtectedLayout>;
 }
